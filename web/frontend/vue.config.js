f20d13f26d2e88b9a1e4d7cfd5eb4f286eb9af54
Soh1121 <satou.shg@gmail.com>
2020-06-07 22:03:51 +0900

    [fix]整形
    #4

--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  outputDir: '../public/app',
-  publicPath: '/app',
+  outputDir: "../public/app",
+  publicPath: "/app",
   pages: {
-      todo: {
-        entry: 'src/todo/main.js',
-        template: 'templates/base.html',
-        filename: `../../resources/views/spa/todo.blade.php`,
-      },
-      timer: {
-        entry: 'src/timer/main.js',
-        template: 'templates/base.html',
-        filename: `../../resources/views/spa/timer.blade.php`,
-      },
-  },
+    todo: {
+      entry: "src/todo/main.js",
+      template: "templates/base.html",
+      filename: `../../resources/views/spa/todo.blade.php`
+    },
+    timer: {
+      entry: "src/timer/main.js",
+      template: "templates/base.html",
+      filename: `../../resources/views/spa/timer.blade.php`
+    }
+  }
 };
