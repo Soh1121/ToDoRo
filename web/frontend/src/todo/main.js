aa2091b1f14b4a8b20146b126cc278c6e25df292
Soh1121 <satou.shg@gmail.com>
2020-06-12 23:03:52 +0900

    [update]Vuetifyのインストールと設定
    Vuetifyを使うため #12

--- web/frontend/src/todo/main.js
+++ web/frontend/src/todo/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router/router";
-import store from "./store/store";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router/router"
+import store from "./store/store"
+import vuetify from '../plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -5,12 +5,13 @@ module.exports = {
     todo: {
       entry: "src/todo/main.js",
       template: "templates/base.html",
-      filename: `../../resources/views/spa/todo.blade.php`
+      filename: "../../resources/views/spa/todo.blade.php"
     },
     timer: {
       entry: "src/timer/main.js",
       template: "templates/base.html",
-      filename: `../../resources/views/spa/timer.blade.php`
+      filename: "../../resources/views/spa/timer.blade.php"
     }
-  }
+  },
+  transpileDependencies: ["vuetify"]
 };
