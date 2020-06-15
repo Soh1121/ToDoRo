a87b7de10a695f7f2ad7985e552d3d4078abbdd1
Soh1121 <satou.shg@gmail.com>
2020-06-15 21:44:40 +0900

    [update]ファイルのパスを変更
    パス変更により表示できなくなった部分を表示させるため #16

--- web/frontend/src/main.js
+++ web/frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router/router"
 import store from "./store/store"
-import vuetify from '../plugins/vuetify'
+import vuetify from './plugins/vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -3,14 +3,9 @@ module.exports = {
   publicPath: "/app",
   pages: {
     todo: {
-      entry: "src/todo/main.js",
+      entry: "src/main.js",
       template: "templates/base.html",
       filename: "../../resources/views/spa/todo.blade.php"
-    },
-    timer: {
-      entry: "src/timer/main.js",
-      template: "templates/base.html",
-      filename: "../../resources/views/spa/timer.blade.php"
     }
   },
   transpileDependencies: ["vuetify"]
