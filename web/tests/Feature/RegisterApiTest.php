be40aa94cd965b66f1be5a0edf0c86063b484556
Soh1121 <satou.shg@gmail.com>
2020-07-14 08:55:37 +0900

    [update]ルート定義を実施
    会員登録APIを実行できるようにするため #24

---
+++ web/tests/Feature/RegisterApiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class RegisterApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function should_新しいユーザーを作成して返却する()
+    {
+        $data = [
+            'name' => 'user',
+            'email' => 'test@email.com',
+            'password' => 'test1234!',
+            'password_confirmation' => 'test1234!',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+    }
+}
