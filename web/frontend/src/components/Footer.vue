aa7d6d2945a7b25ecd3d8f15abc9fd9e97ac0f3a
Soh1121 <satou.shg@gmail.com>
2020-07-18 23:39:59 +0900

    [fix]linterで整形
    マージするため #34

--- web/frontend/src/components/Footer.vue
+++ web/frontend/src/components/Footer.vue
@@ -2,7 +2,10 @@
   <v-footer absolute>
     <v-card flat tile width="100%" class="grey lighten-4 text-center">
       <v-card-text>
-        <small>Copyright &copy; {{ new Date().getFullYear() }} ToDoRo, All Rights Reserved.</small>
+        <small>
+          Copyright &copy; {{ new Date().getFullYear() }} ToDoRo, All Rights
+          Reserved.
+        </small>
       </v-card-text>
     </v-card>
   </v-footer>
