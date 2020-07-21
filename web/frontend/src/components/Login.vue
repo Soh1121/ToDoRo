d66a51a0c8605cb96e29803eb5d898bbc748689d
Soh1121 <satou.shg@gmail.com>
2020-07-21 23:47:41 +0900

    [update]データの紐付けとパスワードの表示／非表示切り替え
    submitの動作準備とフォームらしさの追求 #35

--- web/frontend/src/components/Login.vue
+++ web/frontend/src/components/Login.vue
@@ -8,12 +8,20 @@
       <v-container>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-text-field label="メールアドレス" required color="orange" />
+            <v-text-field label="メールアドレス" required color="orange" v-model="loginForm.email" />
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-text-field label="パスワード" required color="orange" />
+            <v-text-field
+              label="パスワード"
+              required
+              color="orange"
+              v-model="loginForm.password"
+              :append-icon="loginForm.show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="loginForm.show ? 'text' : 'password'"
+              @click:append="loginForm.show = !loginForm.show"
+            />
           </v-col>
         </v-row>
       </v-container>
@@ -22,22 +30,45 @@
       <v-container>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-text-field label="ユーザー名 *" hint="英数字・ハイフン・アンダーバーを利用可能" required color="orange" />
+            <v-text-field
+              label="ユーザー名 *"
+              hint="英数字・ハイフン・アンダーバーを利用可能"
+              required
+              color="orange"
+              v-model="registerForm.name"
+            />
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-text-field label="メールアドレス *" required color="orange" />
+            <v-text-field label="メールアドレス *" required color="orange" v-model="registerForm.email" />
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-text-field label="パスワード *" hint="英数字・記号を利用可能" required color="orange" />
+            <v-text-field
+              label="パスワード *"
+              hint="英数字・記号を利用可能"
+              required
+              color="orange"
+              v-model="registerForm.password"
+              :append-icon="registerForm.show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="registerForm.show ? 'text' : 'password'"
+              @click:append="registerForm.show = !registerForm.show"
+            />
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-text-field label="パスワード確認 *" required color="orange" />
+            <v-text-field
+              label="パスワード確認 *"
+              required
+              color="orange"
+              v-model="registerForm.password_confirmation"
+              :append-icon="registerForm.show_confirmation ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="registerForm.show_confirmation ? 'text' : 'password'"
+              @click:append="registerForm.show_confirmation = !registerForm.show_confirmation"
+            />
           </v-col>
         </v-row>
         <v-row>
@@ -60,7 +91,20 @@ export default {
   props: ["dialog"],
   data() {
     return {
-      tab: 1
+      tab: 1,
+      loginForm: {
+        email: "",
+        password: "",
+        show: false
+      },
+      registerForm: {
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+        show: false,
+        show_confirmation: false
+      }
     };
   },
   methods: {
