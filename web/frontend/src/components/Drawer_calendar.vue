1f3e246df4be2484c01b3ff0f48400aa0ba15d2c
Soh1121 <satou.shg@gmail.com>
2020-10-13 23:48:45 +0900

    [add]サイドバー下部のコンポーネントを追加
    サイドバーを構築するため #95

---
+++ web/frontend/src/components/Drawer_calendar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-list
+    dense
+    width="100%"
+    class="l-sidebar__inner--bottom"
+  >
+    <v-divider />
+
+    <v-list-item
+      v-for="schedule in schedules"
+      :key="schedule.title"
+      link
+    >
+      <v-list-item-icon>
+        <v-icon>{{ schedule.icon }}</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title>{{ schedule.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      schedules: [
+        { title: "今　日", icon: "mdi-weather-sunny" },
+        { title: "明　日", icon: "mdi-moon-waning-crescent" },
+        { title: "近日中", icon: "mdi-timer" },
+        { title: "いつか", icon: "mdi-calendar-arrow-right" },
+      ],
+    }
+  },
+}
+</script>
