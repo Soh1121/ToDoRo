838f0154a01c322c84465040d515da564d8f636a
Soh1121 <satou.shg@gmail.com>
2020-10-29 22:21:09 +0900

    [add]フォームリクエストを追加
    ユーザーが入力した値をリクエストに含んでいるため #98

---
+++ web/app/Http/Requests/ContextRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContextRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'context_id' => 'integer',
+            'name' => 'max:15',
+        ];
+    }
+}
