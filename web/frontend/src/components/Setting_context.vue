3425ed369b6286cdcd3224677765ce55f7a7ea1c
Soh1121 <satou.shg@gmail.com>
2020-11-29 23:08:33 +0900

    [update]コンテキスト名編集機能を結合
    コンテキスト名を編集するため #136

--- web/frontend/src/components/Setting_context.vue
+++ web/frontend/src/components/Setting_context.vue
@@ -1,62 +1,138 @@
 <template>
   <div class="u-margin__padding--10px0">
     <div class="u-size__width--94per u-align__block--center">
-      <v-dialog v-model="dialog" max-width="600px">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            depressed
-            color="primary"
-            class="u-margin__margin--10px0"
-            v-on="on"
-            v-bind="attrs"
-            @click="open"
-          >
-            追加
-          </v-btn>
-        </template>
-        <ContextAdd />
-      </v-dialog>
+      <template>
+        <v-btn
+          depressed
+          color="primary"
+          class="u-margin__margin--10px0"
+          v-bind="attrs"
+          @click="add"
+        >
+          追加
+        </v-btn>
+      </template>
 
-      <CardList
-        v-bind:mode="context"
-      />
+      <v-data-table
+        :headers="headers"
+        :items="contexts"
+        sort-by="context_id"
+        disable-pagination="true"
+        hide-default-footer="true"
+      >
+        <template v-slot:top>
+          <v-dialog v-model="dialog" max-width="600px">
+            <v-card>
+              <v-card-text>
+                <v-container>
+                  <!-- <v-row
+                    v-if="contextAddErrors"
+                    class="font-weight-bold red--text text--darken-3"
+                  >
+                    <ul v-if="contextAddErrors.email">
+                      <li v-for="msg in loginErrors.email" :key="msg">
+                        {{ msg }}
+                      </li>
+                    </ul>
+                    <ul v-if="loginErrors.password">
+                      <li v-for="msg in loginErrors.password" :key="msg">
+                        {{ msg }}
+                      </li>
+                    </ul>
+                  </v-row> -->
+                  <v-row>
+                    <v-col cols="12" sm="12" md="12">
+                      <v-text-field
+                        label="コンテキスト名"
+                        hint="30文字以内"
+                        required
+                        color="orange"
+                        v-model="contextAddForm.name"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="orange" text @click="close">
+                  キャンセル
+                </v-btn>
+                <v-btn
+                  v-if="isPersistedItem"
+                  color="orange"
+                  dark
+                  @click="update"
+                >
+                  変更
+                </v-btn>
+                <v-btn v-else color="orange" dark @click="create">
+                  追加
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <div v-if="item.name !== '未設定'">
+            <v-icon @click="edit(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon @click="remove(item)">
+              mdi-delete
+            </v-icon>
+          </div>
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
 
 <script>
 import { OK } from "../util";
-import CardList from "./Setting_cardList";
-import ContextAdd from "./Setting_contextAdd";
 import { mapGetters } from "vuex";
 
 export default {
-  components: {
-    CardList,
-    ContextAdd
-  },
   data() {
     return {
+      dialog: false,
+      // 設定画面に表示するコンテキストデータ
       contexts: [
         {
-          id: 0,
-          title: ""
+          context_id: 0,
+          name: ""
         }
-      ]
+      ],
+      // 設定データ表示用のヘッダー
+      headers: [
+        {
+          text: "コンテキスト名",
+          value: "name"
+        },
+        {
+          text: "操作",
+          value: "actions",
+          align: "center",
+          width: "100px"
+        }
+      ],
+      // 設定用のダイアログのフォームデータ
+      contextAddForm: {}
     };
   },
+
   computed: {
     ...mapGetters({
       userId: "auth/user_id",
-      storeContexts: "context/contexts",
-      dialog: "context/display"
-    })
-  },
-  methods: {
-    open() {
-      this.$store.dispatch("context/open");
-    },
+      storeContexts: "context/contexts"
+    }),
 
+    isPersistedItem() {
+      return !!this.contextAddForm.context_id;
+    }
+  },
+
+  methods: {
     async fetch() {
       const route = "/api/contexts/" + this.userId;
       const response = await window.axios.get(route);
@@ -69,14 +145,45 @@ export default {
       let datas = [];
       response.data.data.forEach(function(item) {
         datas.push({
-          id: item.id,
-          title: item.name
+          context_id: item.id,
+          name: item.name
         });
       });
 
       this.contexts = datas;
+    },
+
+    add() {
+      this.contextAddForm = {};
+      this.dialog = true;
+    },
+
+    async create() {
+      await this.$store.dispatch("context/create", [
+        this.userId,
+        this.contextAddForm
+      ]);
+      this.close();
+    },
+
+    edit(item) {
+      this.contextAddForm = item;
+      this.dialog = true;
+    },
+
+    async update() {
+      await this.$store.dispatch("context/update", [
+        this.userId,
+        this.contextAddForm
+      ]);
+      this.close();
+    },
+
+    close() {
+      this.dialog = false;
     }
   },
+
   watch: {
     $route: {
       async handler() {
@@ -90,8 +197,8 @@ export default {
         let datas = [];
         values["data"].forEach(function(item) {
           datas.push({
-            id: item.id,
-            title: item.name
+            context_id: item.id,
+            name: item.name
           });
         });
         this.contexts = datas;
