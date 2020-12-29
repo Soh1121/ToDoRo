00441224c7e54cfc823a4a91fa6af25d9b1d796f
Soh1121 <satou.shg@gmail.com>
2020-12-29 20:26:05 +0900

    [update]モデルの作成
    タスクを作成するため #74

--- web/app/Task.php
+++ web/app/Task.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /** JSONに含まれる属性 */
+    protected $visible = [
+        'id', 'name', 'user_id', 'project_id',
+        'context_id', 'start_date', 'due_date',
+        'term', 'finished', 'done',
+        'timer', 'repeat_id', 'priority',
+    ];
+
+    /**
+     * リレーションシップ
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id', 'users');
+    }
 }
