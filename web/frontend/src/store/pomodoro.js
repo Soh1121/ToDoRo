0f7e598ebbce3d57dc1167b22f8de490d593ed69
Soh1121 <satou.shg@gmail.com>
2021-03-17 22:52:29 +0900

    [fix]タイマースタート処理をactionsへ
    mutationsは値の変更だけ

--- web/frontend/src/store/pomodoro.js
+++ web/frontend/src/store/pomodoro.js
@@ -31,15 +31,20 @@ const mutations = {
     state.playMode = mode;
   },
 
-  setTimerId(state) {
-    state.timerId = setInterval(() => {
-      if (state.time === 0) {
-        state.playMode = "stop";
-        return null;
-      }
-      state.time -= 1;
-    }, 1000);
+  setTimerId(state, timerId) {
+    // state.timerId = setInterval(() => {
+    //   if (state.time === 0) {
+    //     state.playMode = "stop";
+    //     return null;
+    //   }
+    //   state.time -= 1;
+    // }, 1000);
+    state.timerId = timerId;
   },
+
+  decrementTime(state) {
+    state.time -= 1;
+  }
 };
 
 const actions = {
@@ -49,7 +54,14 @@ const actions = {
 
   start(context) {
     context.commit("setPlayMode", "play");
-    context.commit("setTimerId");
+    const timerId = setInterval(() => {
+      if (state.time === 0) {
+        context.commit("setPlayMode", "stop");
+        return null;
+      }
+      context.commit("decrementTime");
+    }, 1000);
+    context.commit("setTimerId", timerId);
   },
 
   pause(context) {
