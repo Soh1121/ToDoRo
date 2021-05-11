f5a87bbc8b060598d62ab97cc1e0656802ed1468
Soh1121 <satou.shg@gmail.com>
2021-05-11 19:03:36 +0900

    [update]タイマー画面遷移時に非ログインを判定
    非ログイン時に処理を飛ばすため #308

--- web/frontend/src/components/task/list/CheckboxAndPlayButton.vue
+++ web/frontend/src/components/task/list/CheckboxAndPlayButton.vue
@@ -48,7 +48,12 @@ export default {
     },
 
     transition(item) {
-      this.$store.dispatch("pomodoro/initPomodoroCount", this.userId);
+      if (this.userId) {
+        console.log("userid");
+      } else {
+        console.log(this.userId);
+      }
+        this.$store.dispatch("pomodoro/initPomodoroCount", this.userId);
       this.$store.dispatch("pomodoro/setStateTime", item.timer);
       // this.$store.dispatch("pomodoro/setStateTime", 15);
       this.$router.push({ name: "Timer", params: { task: item } });
