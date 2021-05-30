c09a99916bd3e91e06dd188aad445f37caa2a801
Soh1121 <satou.shg@gmail.com>
2021-05-30 16:13:03 +0900

    [update]タイマー全体を中央寄せ
    タイマーをなるべくコンパクトにするため #323

--- web/frontend/src/components/timer/button/ContinueButton.vue
+++ web/frontend/src/components/timer/button/ContinueButton.vue
@@ -4,7 +4,7 @@
     width="100px"
     height="50px"
     :color="color"
-    class="u-margin__margin--t50px u-margin__margin--r50px"
+    class="u-margin__margin--10px0 u-margin__margin--r50px"
     @click="start"
   >
     続ける
