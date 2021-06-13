06bb972596ba7a1da1ce3796ff842df3d10ca473
Soh1121 <satou.shg@gmail.com>
2021-06-13 15:58:43 +0900

    [update]確認ダイアログを作成
    タイマー動作中に別タスクを行おうとしたら確認を行うため #327

--- web/frontend/src/components/task/TaskConfirmation.vue
+++ web/frontend/src/components/task/TaskConfirmation.vue
@@ -1,7 +1,34 @@
 <template>
   <v-card>
+    <v-card-title>
+      ポモドーロタイマーが動いているタスクがあります
+    </v-card-title>
     <v-card-text>
-      <p>テスト</p>
+      現在「{{ taskName }}」のタスクでポモドーロタイマーが動いています。<br>
+      新しくタスクをセットしてタイマー画面に移りますか？
     </v-card-text>
+    <div class="u-margin__padding--b28px">
+      <v-card-actions>
+        <v-btn width="100%" max-width="300px" large color="primary" class="u-margin__margin--lrauto">現在のタスクを進める</v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        <v-btn width="100%" max-width="300px" large color="primary" class="u-margin__margin--lrauto">新しいタスクを行う</v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        <v-btn width="100%" max-width="300px" large color="primary" class="u-margin__margin--lrauto" outlined>キャンセル</v-btn>
+      </v-card-actions>
+    </div>
   </v-card>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default ({
+  computed: {
+    ...mapState({
+      taskName: state => state.pomodoro.taskName
+    })
+  }
+})
+</script>
