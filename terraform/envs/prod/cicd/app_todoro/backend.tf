463c0b60d31e7818e5be9bd04e9046a4a58e74b2
Soh1121 <satou.shg@gmail.com>
2021-07-18 21:21:55 +0900

    [fix]tfstateの名前を修正
    appと同様の名前をつけていたため #349

--- terraform/envs/prod/cicd/app_todoro/backend.tf
+++ terraform/envs/prod/cicd/app_todoro/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "todoro-tfstate"
-    key    = "todoro/prod/app/todoro_v1.0.1.tfstate"
+    key    = "todoro/prod/cicd/app_todoro_v1.0.1.tfstate"
     region = "ap-northeast-1"
   }
 }
