94ec7c2716b286326261e12da7cd294db4ab2b4a
Soh1121 <satou.shg@gmail.com>
2021-07-22 18:03:45 +0900

    [update]IAMロールの作成
    circleci用アカウントに付与するため #355

--- terraform/envs/prod/cicd/app_todoro/iam.tf
+++ terraform/envs/prod/cicd/app_todoro/iam.tf
@@ -5,3 +5,29 @@ resource "aws_iam_user" "circleci" {
     Name = "${local.name_prefix}-${local.service_name}-circleci"
   }
 }
+
+resource "aws_iam_role" "deployer" {
+  name = "${local.name_prefix}-${local.service_name}-deployer"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "sts:AssumeRole",
+            "sts:TagSession"
+          ],
+          "Principal": {
+            "AWS": aws_iam_user.circleci.arn
+          }
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-deployer"
+  }
+}
