f6affef90e1623ab59d6b8dbd1efac11814b2a81
Soh1121 <satou.shg@gmail.com>
2021-07-25 14:43:13 +0900

    [update]サブネットの作成
    terraformでVPCを構築するため #364

--- terraform/envs/prod/network/main/variables.tf
+++ terraform/envs/prod/network/main/variables.tf
@@ -2,3 +2,20 @@ variable "vpc_cider" {
 	type = string
 	default = "172.32.0.0/16"
 }
+
+variable "azs" {
+  type = map(object({
+	  public_cidr = string
+	  private_cidr = string
+  }))
+  default = {
+    a = {
+      public_cidr = "172.32.0.0/20"
+      private_cidr = "172.32.48.0/20"
+    },
+    c = {
+	    public_cidr = "172.32.16.0/20"
+	    private_cidr = "172.32.64.0/20"
+    }
+  }
+}
