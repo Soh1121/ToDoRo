bb3bdc204cdaa0eac20efa98807f5fe9f9090c96
Soh1121 <satou.shg@gmail.com>
2021-07-25 22:28:13 +0900

    [add]タスク実行ロールの作成
    terraformでECSを構築するため #368

---
+++ terraform/envs/prod/app/todoro/iam.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${local.name_prefix}-${local.service_name}-ecs-task-execution"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-ecs-task-execution"
+  }
+}
+
+data "aws_iam_policy" "ecs_task_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = data.aws_iam_policy.ecs_task_execution.arn
+}
