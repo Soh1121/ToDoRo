342d52b9e316bdf204a065ac30dd2b7a36d4e307
Soh1121 <satou.shg@gmail.com>
2021-07-26 20:30:02 +0900

    [update]タスクロールの作成
    コンテナの中に入れるようにするため #368

--- terraform/envs/prod/app/todoro/iam.tf
+++ terraform/envs/prod/app/todoro/iam.tf
@@ -57,3 +57,49 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = aws_iam_policy.ssm.arn
 }
+
+resource "aws_iam_role" "ecs_task" {
+  name = "${local.name_prefix}-${local.service_name}-ecs-task"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-ecs-task"
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_task_ssm" {
+  name = "ssm"
+  role = aws_iam_role.ecs_task.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssmmessages:CreateControlChannel",
+            "ssmmessages:CreateDataChannel",
+            "ssmmessages:OpenControlChannel",
+            "ssmmessages:OpenDataChannel"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
+}
