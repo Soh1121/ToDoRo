83cc19ab32d32a6c5ee2dab43a1c041dcf7e9807
Soh1121 <satou.shg@gmail.com>
2021-07-26 21:12:21 +0900

    [update]タスク定義の作成
    terraformでFargateを起動するため #368

--- terraform/envs/prod/app/todoro/ecs.tf
+++ terraform/envs/prod/app/todoro/ecs.tf
@@ -10,3 +10,99 @@ resource "aws_ecs_cluster" "this" {
     Name = "${local.name_prefix}-${local.service_name}"
   }
 }
+
+resource "aws_ecs_task_definition" "this" {
+  family = "${local.name_prefix}-${local.service_name}"
+
+  task_role_arn = aws_iam_role.ecs_task.arn
+
+  network_mode = "awsvpc"
+
+  requires_compatibilities = [
+    "FARGATE",
+  ]
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+
+  memory = "512"
+  cpu    = "256"
+
+  container_definitions = jsonencode(
+    [
+      {
+        name  = "nginx"
+        image = "${module.nginx.ecr_repository_this_repository_url}:latest"
+
+        portMappings = [
+          {
+            hostPort      = 80
+            containerPort = 80
+          }
+        ]
+
+        environment = []
+        secrets     = []
+
+        dependsOn = [
+          {
+            containerName = "php"
+            condition     = "START"
+          }
+        ]
+
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = "/ecs/${local.name_prefix}-${(local.service_name)}/nginx"
+            awslogs-region        = data.aws_region.current.id
+            awslogs-stream-prefix = "ecs"
+          }
+        }
+      },
+      {
+        name  = "php"
+        image = "${module.php.ecr_repository_this_repository_url}:latest"
+
+        portMappings = [
+          {
+            hostPort      = 9000
+            containerPort = 9000
+          }
+        ]
+
+        environment = []
+        secrets = [
+          {
+            name      = "APP_KEY"
+            valueFrom = "/${local.service_name}/${local.env_name}/${local.service_name}/APP_KEY"
+          }
+        ]
+
+        # mountPoints = [
+        #   {
+        #     # 自分の環境に合わせて
+        #     containerPath = ""
+        #     sourceVolume = ""
+        #   }
+        # ]
+
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = "/ecs/${local.name_prefix}-${(local.service_name)}/php"
+            awslogs-region        = data.aws_region.current.id
+            awslogs-stream-prefix = "ecs"
+          }
+        }
+      }
+    ]
+  )
+
+  volume {
+    name = "nginx-laravel-spa"
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}"
+  }
+}
