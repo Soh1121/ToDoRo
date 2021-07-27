5ea108a58c735a032d155ad32b68c650a11d411a
Soh1121 <satou.shg@gmail.com>
2021-07-27 22:01:22 +0900

    [fix]

--- terraform/envs/prod/routing/todoro_tokyo/alb.tf
+++ terraform/envs/prod/routing/todoro_tokyo/alb.tf
@@ -36,13 +36,8 @@ resource "aws_lb_listener" "https" {
   ssl_policy        = "ELBSecurityPolicy-2016-08"
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.todoro.arn
   }
 }
 
