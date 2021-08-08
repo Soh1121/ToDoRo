da88bab9efa0b9a554fa6af5176e5ab85ab1bb53
Soh1121 <satou.shg@gmail.com>
2021-08-08 15:25:16 +0900

    [update]RDS用セキュリティグループの作成
    RDSを構築するため #375

--- terraform/envs/prod/network/main/outputs.tf
+++ terraform/envs/prod/network/main/outputs.tf
@@ -6,6 +6,10 @@ output "security_group_vpc_id" {
   value = aws_security_group.vpc.id
 }
 
+output "security_group_db_todoro_id" {
+  value = aws_security_group.db_todoro.id
+}
+
 output "subnet_public" {
   value = aws_subnet.public
 }
